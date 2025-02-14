1fa74c4ba0d8872701e57003643fd88dae79d048
victory1303 <mnzvictory@gmail.com>
2025-02-14 11:26:34 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import numpy as np
-import streamlit as st
-
+import streamlit as st     
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.preprocessing import StandardScaler
 import joblib
 import tkinter as tk
 
