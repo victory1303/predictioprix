92e60cfc335041bb6837920bc06be22377e31fb1
victory1303 <mnzvictory@gmail.com>
2025-02-14 11:23:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.preprocessing import StandardScaler
 import joblib
 import tkinter as tk
 
